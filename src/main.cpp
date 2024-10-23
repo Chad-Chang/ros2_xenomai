fe8c4b353d6fd2e3228e5087559ff506ce9633d7
chad <dlckdals123ckd@gmail.com>
2024-10-23 19:16:17 +0900

    delay check

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-#define SHM_NAME "/my_shared_memory"
+#define SHM_NAME "/mcl_420"
 #define SHM_SIZE sizeof(SharedData)
 
 typedef struct {
@@ -31,57 +31,56 @@ typedef struct {
     float load_pos;
     float motor_vel;
     float load_vel;
-    // char name[20];
 } SharedData;
 
- using namespace std::chrono_literals;
+using namespace std::chrono_literals;
 
- // cpu 코어당 상용량 확인 : top , 1
- double cnt = 0 ;
+// cpu 코어당 상용량 확인 : top , 1
+double cnt = 0 ;
 
 
- class HelloworldPublisher : public rclcpp::Node
- {
- public:
-   HelloworldPublisher()
-   : Node("helloworld_publisher"), count_(0)
-   {
-     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(1)).reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
-    //  auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(1), rmw_qos_profile_sensor_data);//.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
-     helloworld_publisher_ = this->create_publisher<std_msgs::msg::String>(
-       "helloworld", qos_profile);
-    //  timer_ = this->create_wall_timer(
-    //    1s, std::bind(&HelloworldPublisher::publish_helloworld_msg, this));
-   }
-  void publish_helloworld_msg()
+class HelloworldPublisher : public rclcpp::Node
+{
+public:
+  HelloworldPublisher()
+  : Node("helloworld_publisher"), count_(0)
   {
-    auto msg = std_msgs::msg::String();
-    msg.data = "Cello Worldsadfasdfasdfasfadsfasdfasdfasdfasdfasfasdfsadf sdafasdfa sdf asdfasf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf adfsadfa sdf asdf asdf asdf asdf asdf wef eq vfevczxcvaf verfvsad: " + std::to_string(count_++);
-    helloworld_publisher_->publish(msg);
-    // RCLCPP_INFO(this->get_logger(), "Published message: '%s' and real count = %f", msg.data.c_str(),cnt);
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(1)).reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+    helloworld_publisher_ = this->create_publisher<std_msgs::msg::String>(
+      "helloworld", qos_profile);
+  //  timer_ = this->create_wall_timer(
+  //    1s, std::bind(&HelloworldPublisher::publish_helloworld_msg, this));
   }
- private:
-   
-   rclcpp::TimerBase::SharedPtr timer_;
-   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr helloworld_publisher_;
-   size_t count_;
- };
+  
+void publish_helloworld_msg()
+{
+  auto msg = std_msgs::msg::String();
+  msg.data = " " + std::to_string(count_++);
+  helloworld_publisher_->publish(msg);
+  // RCLCPP_INFO(this->get_logger(), "Published message: '%s' and real count = %f", msg.data.c_str(),cnt);
+}
+private:
+  
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr helloworld_publisher_;
+  size_t count_;
+};
+
+//
+
+#define RT_PERIOD_MS 1 //1msec
+#define CPU_AFFINITY_NUM 0 // 쓰레드를 특정 CPU코어에 고정 -> 어떤 코어를 사용할지 선택 가능
+
+pthread_t rt;
+pthread_mutex_t data_mut = PTHREAD_MUTEX_INITIALIZER; // main thread와 데이터 겹치지 않게 만들어주기-> stack영역의 데이터 변경 불가
 
 
- #define RT_PERIOD_MS 1 //1msec
- #define CPU_AFFINITY_NUM 0 // 쓰레드를 특정 CPU코어에 고정 -> 어떤 코어를 사용할지 선택 가능
-
- pthread_t rt;
- pthread_mutex_t data_mut = PTHREAD_MUTEX_INITIALIZER; // main thread와 데이터 겹치지 않게 만들어주기-> stack영역의 데이터 변경 불가
-
-
- int sigMainKill = 0;
- static void cleanup(void);
+int sigMainKill = 0;
+static void cleanup(void);
 //  static void *realtime_thread(void *arg);
 
 
-
-
+/*loop time측정용임.*/
 long t1 = 0;
 long ts = 0;
 long old_t1= 0;
@@ -91,85 +90,98 @@ int tfd= 0;
 int err;
 int shm_fd;
 void *shm_ptr;
+uint64_t ticks;
+uint32_t overrun = 0;
 
+/*공유 메모리 변수*/
 SharedData data;
 
 // C 스타일의 함수로 전달할 함수를 정의
-void *realtime_thread(void *arg)
+void *realtime_thread(void *arg) // ros2의 통신을 사용할때는 argument를 instance를 가져와서 사용함.
 {
-  (void) arg;
-    // auto node_ptr2 = static_cast<std::shared_ptr<HelloworldPublisher>*>(arg);
-    // rclcpp::spin(*node_ptr);  // ROS2 노드 실행
+  
+  /*ros2통신 사용할때 on*/ 
+    // auto node_ptr2 = static_cast<std::shared_ptr<HelloworldPublisher>*>(arg); 
+    // rclcpp::spin(*node_ptr);  // ROS2 노드 실행 => 노드가 메시지, 서비스, 타이머
     // return NULL;
 
-    int tfd; // desired timer
-    struct timespec trt; // 실제 타이머
-    struct itimerspec timer_conf; //
-    struct timespec expected; //
-    clock_gettime(CLOCK_MONOTONIC, &expected);  // 고정된 기준으로 증가하는 시간 의미 
-    tfd = timerfd_create(CLOCK_MONOTONIC, 0); //create timer descriptor
-    if(tfd == -1) error(1, errno, "timerfd_create()");
-    timer_conf.it_value = expected; //from now
-    timer_conf.it_interval.tv_sec = 0;
-    timer_conf.it_interval.tv_nsec = RT_PERIOD_MS*1000000; //interval with RT_PERIOD_MS
-    // 타이머의 interval time을 설정해줌 => timer configuration을 설정해주는 부분 - nano sec단위로 해주나봄.
-    int err = timerfd_settime(tfd, TFD_TIMER_ABSTIME, &timer_conf, NULL); //set the timer descriptor
-    if(err) error(1, errno, "timerfd_setting()");
+  /*공유 메모리로 통신할때*/
+  (void) arg;  
+  int tfd; // desired timer
+  struct timespec trt; // 실제 타이머
+  struct itimerspec timer_conf; //
+  struct timespec expected; //
+  clock_gettime(CLOCK_MONOTONIC, &expected); 
+  tfd = timerfd_create(CLOCK_MONOTONIC, 0); //create timer descriptor
+  if(tfd == -1) error(1, errno, "timerfd_create()");
 
+  //타이머 configuration 설정
+  timer_conf.it_value = expected; //from now
+  timer_conf.it_interval.tv_sec = 0;
+  timer_conf.it_interval.tv_nsec = RT_PERIOD_MS*1000000; //interval with RT_PERIOD_MS
+  
+  // 타이머 세팅 instance에 적용
+  int err = timerfd_settime(tfd, TFD_TIMER_ABSTIME, &timer_conf, NULL); //set the timer descriptor
+  if(err) error(1, errno, "timerfd_setting()");
 
-    uint64_t ticks;
-    uint32_t overrun = 0;
-    while(!sigMainKill)
+  
+  while(!sigMainKill) // cnt가 어느정도 이상되면 main에서 sigMainkill을 true로 바꿔줌
+  {
+    // clock_gettime(CLOCK_REALTIME, &trt); //get the system time
+    err = read(tfd, &ticks,sizeof(ticks));  //해당 타이머가 설정한 간격으로 발생한 타이머 틱수를 읽어옴.
+    clock_gettime(CLOCK_MONOTONIC, &trt); //get the system time
+    /*
+    1. 타이머 이벤트 대기 : timer된 sampling마다 타이머가 생존하고, 이후에 타이머가 만료됨-> 만료될때까지 block함. 
+    만료되면 읽기 작업 완료 -> ticks값에 타이머 만료된 횟수 저장
+    2. 타이머가 한번이상 만료되면 일정시간 동안 처리 실패-> 오버런.
+    3. 정확한 주기 보장*/ 
+    old_t1 = t1;
+    t1 = trt.tv_nsec;
+    ts = trt.tv_sec;
+    delta_t1 = t1 - old_t1;
+    sampling_ms = (double)delta_t1*0.000001;
+    double jitter = sampling_ms - 1.000; 
+
+    if(ticks>1) overrun += ticks - 1; 
+    // if(jitter >1) overrun +=1; // 차이가 1ms이상
+
+    /*통신으로 설정할 때*/
+    // (*node_ptr2)->publish_helloworld_msg();
+
+    cnt +=0.001;
+    
+    if(err<0) error(1,errno, "read()");
+
+    data.motor_num = 1;
+    data.motor_pos = cnt;
+    data.time_stamp = (double) trt.tv_sec + (trt.tv_nsec/1e9);
+    memcpy(shm_ptr, &data, sizeof(SharedData));
+
+  /*shared memory 전달한 값 확인 */
+    // printf("Data written to shared memory: motor_num=%d, motor_pos=%.4f, time_stamp = %.4f\n",
+    //       data.motor_num, data.motor_pos, data.time_stamp);
+  /* 루프타임 확인 */
+    printf("PERIODIC TIME --- %.4f, Jitter --- %+.4f, OVERRUN --- %d \r\n", sampling_ms, jitter, overrun);
+    
+    if(!pthread_mutex_trylock(&data_mut))
     {
-      // clock_gettime(CLOCK_REALTIME, &trt); //get the system time
-      clock_gettime(CLOCK_MONOTONIC, &trt); //get the system time
-      old_t1 = t1;
-
-      t1 = trt.tv_nsec;
-      ts = trt.tv_sec;
-      delta_t1 = t1 - old_t1;
-      sampling_ms = (double)delta_t1*0.000001;
-      double jitter = sampling_ms - 1.000; 
-
-      // if(ticks>1) overrun += ticks - 1; 
-      if(jitter >1) overrun +=1;
-
-      // (*node_ptr2)->publish_helloworld_msg();
-      cnt +=0.001;
-      err = read(tfd, &ticks,sizeof(ticks));  // 이게 RT를 유지해주는 놈임.
-      
-      
-      if(err<0) error(1,errno, "read()");
-
-      data.motor_num = 1;
-      data.motor_pos = cnt;
-      data.time_stamp = (double) trt.tv_sec + (trt.tv_nsec/1e9);
-      memcpy(shm_ptr, &data, sizeof(SharedData));
-
-      printf("Data written to shared memory: motor_num=%d, motor_pos=%.4f, time_stamp = %.4f\n",
-            data.motor_num, data.motor_pos, data.time_stamp);
-
-      // printf("PERIODIC TIME --- %.4f, Jitter --- %+.4f, OVERRUN --- %d \r\n", sampling_ms, jitter, overrun);
-      
-      if(!pthread_mutex_trylock(&data_mut))
-      {
-          pthread_mutex_unlock(&data_mut);
-      }
+        pthread_mutex_unlock(&data_mut);
     }
+  }
+  
+  // 매핑 해제 및 공유 메모리 닫기
+  munmap(shm_ptr, SHM_SIZE);
+  close(shm_fd);
 
-    // 매핑 해제 및 공유 메모리 닫기
-    munmap(shm_ptr, SHM_SIZE);
-    close(shm_fd);
-
-    pthread_exit(NULL); //while loop 종료 -> thread 종료
-    return NULL;
+  pthread_exit(NULL); //while loop 종료 -> thread 종료
+  return NULL;
 
     
 }
 
 int main(int argc, char *argv[])
 {
-    
+    mlockall(MCL_CURRENT|MCL_FUTURE); 
     // 공유 메모리 생성 및 열기
     shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
     if (shm_fd == -1) {
@@ -189,14 +201,13 @@ int main(int argc, char *argv[])
         perror("mmap");
         // SharedData data;
         exit(EXIT_FAILURE);
-    }
+    }   
 
-    
-
-   
     // (void) argc; (void) argv;
     rclcpp::init(argc, argv);
     // mlockall(MCL_CURRENT|MCL_FUTURE);
+    
+    /*xenomai RT thread 만들기*/
     pthread_attr_t rtattr;
     sigset_t set;
     cpu_set_t cpus;
@@ -225,10 +236,13 @@ int main(int argc, char *argv[])
 
     ret = pthread_create(&rt, &rtattr, realtime_thread, &node_ptr); //create RT thread
     if(ret) error(1, ret, "pthread_create(realtime_thread)");
+
+    
     pthread_attr_destroy(&rtattr);
     while(cnt < 10);
+    /*끝*/
     sigMainKill = 1;
-    usleep(2000);
+    usleep(1000);
     cleanup();
     rclcpp::shutdown();
   return 0;
--- src/subs.cpp
+++ src/subs.cpp
@@ -1,22 +1,22 @@
 #include <functional>
 #include <memory>
- #include <stdio.h>
- #include <stdlib.h>
- #include <unistd.h> //C POSIX lib header. Header for accessing to the POSIX OS API
- #include <fcntl.h> //C POSIX lib header. Header for opening and locking files and processing other tasks.
- #include <signal.h> //Header for signal processing
- #include <sys/timerfd.h> //
- #include <string.h>
- #include <malloc.h> //Memory allocation
- #include <pthread.h> //Header for using Thread operation from xenomai
- #include <error.h> //
- #include <errno.h> //Header for defining macros for reporting and retrieving error conditions using the symbol 'errno'
- #include <sys/mman.h> //
- #include <rtdm/ipc.h> //
- #include <iostream>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> //C POSIX lib header. Header for accessing to the POSIX OS API
+#include <fcntl.h> //C POSIX lib header. Header for opening and locking files and processing other tasks.
+#include <signal.h> //Header for signal processing
+#include <sys/timerfd.h> //
+#include <string.h>
+#include <malloc.h> //Memory allocation
+#include <pthread.h> //Header for using Thread operation from xenomai
+#include <error.h> //
+#include <errno.h> //Header for defining macros for reporting and retrieving error conditions using the symbol 'errno'
+#include <sys/mman.h> //
+#include <rtdm/ipc.h> //
+#include <iostream>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
@@ -26,16 +26,11 @@ using namespace std::chrono_literals;
 pthread_t rt;
 pthread_mutex_t data_mut = PTHREAD_MUTEX_INITIALIZER; // main thread와 데이터 겹치지 않게 만들어주기-> stack영역의 데이터 변경 불가
 
-
-#define SHM_NAME "/my_shared_memory"
+#define SHM_NAME "/mcl_420"
 #define SHM_SIZE sizeof(SharedData)
-
-
 
 // cpu 코어당 상용량 확인 : top , 1
 double cnt = 0 ;
-
-int sigMainKill = 0;
 static void cleanup(void);
 
 typedef struct {
@@ -56,36 +51,47 @@ public:
   : Node("Helloworld_subscriber")
   {
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(1)).reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
-    
     helloworld_subscriber_ = this->create_subscription<std_msgs::msg::String>(
       "helloworld",
       qos_profile,
       std::bind(&HelloworldSubscriber::subscribe_topic_message, this, _1));
-  
   }
-
 private:
-  
   void subscribe_topic_message(const std_msgs::msg::String::SharedPtr msg) //const
   {
     (void) msg;
-    
-
+    // RCLCPP_INFO(this->get_logger(), "Received message: '%s'", msg->data.c_str());
   }
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr helloworld_subscriber_;
-  
-  
-  
 };
-bool signal_end= 0;
+bool sigMainKill= 0;
+
+uint64_t ticks;
+int shm_fd;
+void *shm_ptr;
+uint32_t overrun = 0;
+long t1 = 0;
+long ts = 0;
+long old_t1= 0;
+long delta_t1= 0;
+double sampling_ms= 0;
+
 void *realtime_thread(void *arg)
 {
+
+  (void) arg;
+  // auto node_ptr2 = static_cast<std::shared_ptr<HelloworldSubscriber>*>(arg);
+  // 이거 실행하면 block됨.
+
+
+/*타이머 설정*/
+  int tfd; // clock instance
   struct timespec trt; //
-  int tfd;
   struct itimerspec timer_conf; //
   struct timespec expected; //
   clock_gettime(CLOCK_MONOTONIC, &expected);
-//RT thread 내부에서 작동하는 timer 생성
+
+  /*RT thread 내부에서 작동하는 timer 생성*/
   tfd = timerfd_create(CLOCK_MONOTONIC, 0); 
   if(tfd == -1) error(1, errno, "timerfd_create()");
 
@@ -94,24 +100,54 @@ void *realtime_thread(void *arg)
   timer_conf.it_interval.tv_nsec = RT_PERIOD_MS*1000000; //interval with RT_PERIOD_MS
 
   //timer 설정 : read함수의 return 주기 : sampling time
-  // int err = timerfd_settime(tfd, TFD_TIMER_ABSTIME, &timer_conf, NULL); 
   int err = timerfd_settime(tfd, TFD_TIMER_ABSTIME, &timer_conf, NULL); 
   if(err) error(1, errno, "timerfd_setting()");
-   
-  // uint32_t overrun = 0;
-  uint64_t ticks;
 
-  (void) arg;
-  int shm_fd;
-  void *shm_ptr;
+  while(!sigMainKill)
+  {
+    err = read(tfd, &ticks,sizeof(ticks)); // RT를 유지해주는 놈.
+    clock_gettime(CLOCK_MONOTONIC, &trt);
+    cnt += 0.001;
+    old_t1 = t1;
+    t1 = trt.tv_nsec;
+    ts = trt.tv_sec;
+    delta_t1 = t1 - old_t1;
+    sampling_ms = (double)delta_t1*0.000001;
+    double jitter = sampling_ms - 1.000; 
 
+    if(ticks>1) overrun += ticks - 1;
+
+    double curr_time=(double) trt.tv_sec + (trt.tv_nsec/1e9);
+    printf("read shared memory: delay = %+.4f, jitter = %+.4f, OVERRUN = %d \n",
+           curr_time-data->time_stamp ,jitter, overrun);
+
+    // printf("PERIODIC TIME --- %.4f, Jitter --- %+.4f, OVERRUN --- %d \r\n", sampling_ms, jitter, overrun);
+  if(!pthread_mutex_trylock(&data_mut))
+    {
+        pthread_mutex_unlock(&data_mut);
+    }
+  }  // ret = pthread_create(&rt, &rtattr, realtime_thread, NULL); //create RT thread
+
+  pthread_exit(NULL); //while loop 종료 -> thread 종료
+
+  return NULL;
+}
+
+//공유메모리 세팅은 반드시 main문에서 선언해야함.
+int main(int argc, char * argv[])
+{
+  // (void) argc;  (void) argv; 
+  rclcpp::init(argc, argv);
+  mlockall(MCL_CURRENT|MCL_FUTURE); 
+
+ 
+/*공유 메모리 설정*/
   // 공유 메모리 열기
   shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
   if (shm_fd == -1) {
       perror("shm_open");
       exit(EXIT_FAILURE);
   }
-
   // 공유 메모리 매핑
   shm_ptr = mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
   if (shm_ptr == MAP_FAILED) {
@@ -120,76 +156,53 @@ void *realtime_thread(void *arg)
   }
   data = (SharedData *)shm_ptr;
   // 공유 메모리에서 구조체 읽기
-  while(!signal_end)
-  {
-    clock_gettime(CLOCK_MONOTONIC, &trt);
-
-    err = read(tfd, &ticks,sizeof(ticks)); // RT를 유지해주는 놈.
-    double curr_time=(double) trt.tv_sec + (trt.tv_nsec/1e9);
-    // double curr_time =0;
-    printf("Data read from shared memory:current = %.4f, delay = %.4f, motor_num=%d, motor_pos=%.4f\n",
-           curr_time, curr_time-data->time_stamp ,data->motor_num, data->motor_pos);
-  }
-// 매핑 해제 및 공유 메모리 닫기
-  munmap(shm_ptr, SHM_SIZE);
-  close(shm_fd);
-  // auto node_ptr2 = static_cast<std::shared_ptr<HelloworldSubscriber>*>(arg);
-  
-  // rclcpp::spin(*node_ptr2);
-
-
-  printf("rt ending");
-  pthread_exit(NULL); //while loop 종료 -> thread 종료
-
-  return NULL;
-}
-
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
   
   pthread_attr_t rtattr;
-    sigset_t set;
-    cpu_set_t cpus;
-    sigemptyset(&set);
-    sigaddset(&set, SIGINT);
-    sigaddset(&set, SIGTERM);
-    sigaddset(&set, SIGHUP);
-    pthread_sigmask(SIG_BLOCK, &set, NULL);
-    struct sched_param p;
-    int ret;
-    ret = pthread_attr_init(&rtattr);
-    if(ret) error(1, ret, "pthread_attr_int()");
-    ret = pthread_attr_setinheritsched(&rtattr, PTHREAD_EXPLICIT_SCHED);
-    if(ret) error(1, ret, "pthread_attr_setinheritsched()");
-    ret = pthread_attr_setschedpolicy(&rtattr, SCHED_FIFO);
-    if(ret) error(1, ret, "pthread_attr_setschedpolicy()");
-    p.sched_priority = 99;
-    ret = pthread_attr_setschedparam(&rtattr, &p);
-    if(ret) error(1, ret, "pthread_attr_setschedparam()");
-    CPU_ZERO(&cpus);
-    CPU_SET(CPU_AFFINITY_NUM, &cpus);
-    ret = pthread_attr_setaffinity_np(&rtattr, sizeof(cpus), &cpus); //give cpu affinity to be used to calculate for the RT thread
-    if(ret) error(1, ret, "pthread_attr_setaffinity_np()");
+  sigset_t set;
+  cpu_set_t cpus;
+  sigemptyset(&set);
+  sigaddset(&set, SIGINT);
+  sigaddset(&set, SIGTERM);
+  sigaddset(&set, SIGHUP);
+  pthread_sigmask(SIG_BLOCK, &set, NULL);
+  struct sched_param p;
+  int ret;
+  ret = pthread_attr_init(&rtattr);
+  if(ret) error(1, ret, "pthread_attr_int()");
+  ret = pthread_attr_setinheritsched(&rtattr, PTHREAD_EXPLICIT_SCHED);
+  if(ret) error(1, ret, "pthread_attr_setinheritsched()");
+  ret = pthread_attr_setschedpolicy(&rtattr, SCHED_FIFO);
+  if(ret) error(1, ret, "pthread_attr_setschedpolicy()");
+  p.sched_priority = 99;
+  ret = pthread_attr_setschedparam(&rtattr, &p);
+  if(ret) error(1, ret, "pthread_attr_setschedparam()");
+  CPU_ZERO(&cpus);
+  CPU_SET(CPU_AFFINITY_NUM, &cpus);
+  ret = pthread_attr_setaffinity_np(&rtattr, sizeof(cpus), &cpus); //give cpu affinity to be used to calculate for the RT thread
+  if(ret) error(1, ret, "pthread_attr_setaffinity_np()");
 
-    auto node = std::make_shared<HelloworldSubscriber>();
-    // ret = pthread_create(&rt, &rtattr, realtime_thread, &node); //create RT thread
-    ret = pthread_create(&rt, &rtattr, realtime_thread, NULL); //create RT thread
-    if(ret) error(1, ret, "pthread_create(realtime_thread)");
-    pthread_attr_destroy(&rtattr);
-    
-    while(data->motor_pos < 10.001);
-    printf("rt ending");
-    signal_end =1 ;
-    usleep(2000);
-    
-    cleanup();
-  rclcpp::shutdown();
+  auto node = std::make_shared<HelloworldSubscriber>();
+  // rclcpp::spin(node);
+  ret = pthread_create(&rt, &rtattr, realtime_thread, &node); //create RT thread
+  // ret = pthread_create(&rt, &rtattr, realtime_thread, NULL); //create RT thread
+  if(ret) error(1, ret, "pthread_create(realtime_thread)");
+  
+  pthread_attr_destroy(&rtattr);
+  while(data->motor_pos < 10.001);
+  // while(cnt <10);
+  printf("rt ending");
+
+  sigMainKill =1 ;
+  usleep(1000);
+  cleanup();
+  // rclcpp::shutdown();
   return 0;
 }
 
  static void cleanup(void)
  {
-     pthread_cancel(rt);
-     pthread_join(rt, NULL);
+    // shm_unlink(SHM_NAME);
+    pthread_cancel(rt);
+    pthread_join(rt, NULL);
+     
  }
